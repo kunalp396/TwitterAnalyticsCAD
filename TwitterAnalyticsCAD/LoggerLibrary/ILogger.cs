a29f7c88a08f46c7e620d80b935616886ee35f98
kunal <kunalpatil2.10.1988@gmail.com>
2016-12-03 23:43:09 +0000

    Logger Library Changes

---
+++ TwitterAnalyticsCAD/LoggerLibrary/ILogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterAnalyticsCommon
+{
+    public interface ILogger
+    {
+         void Log(string message);
+
+         void Log(string message, LOGLEVELS loglevel);
+    }
+
+    public enum LOGLEVELS
+    {
+        ERROR=-1,
+        DEBUG=0,
+        INFO=1
+    }
+}
---
+++ TwitterAnalyticsCAD/LoggerLibrary/LoggerFactory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterAnalyticsCommon
+{
+    public class LoggerFactory<T>
+    {
+        private static object mLock;
+        private static LoggerFactory<T> mLogger = null;
+
+        private LoggerFactory()
+        {
+            mLock = new object();
+        }
+        public static LoggerFactory<T> Instance
+        {
+            get
+            {
+                if (mLogger == null)
+                {
+                    lock (mLock)
+                    {
+                        mLogger = new LoggerFactory<T>();
+                    }
+                }
+                return mLogger;
+            }
+        }
+        static readonly Dictionary<Type, Func<T>> _dict
+             = new Dictionary<Type, Func<T>>();
+
+        public static T Create(Type loggerType)
+        {
+            Func<T> constructor = null;
+
+            if (_dict.TryGetValue(loggerType, out constructor))
+                return constructor();
+
+            throw new ArgumentException("No type registered for this id");
+        }
+
+        public static void Register(Type loggerType, Func<T> ctor)
+        {
+            _dict.Add(loggerType, ctor);
+        }
+    }
+}
